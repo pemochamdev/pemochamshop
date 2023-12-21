8a51784b4ca130d38dea0891563cbb3b5bdf9fca
pemochamdev <pemochamdev@gmail.com>
2023-12-21 14:34:35 +0000

    Calculate Tax & Grand Total

--- carts/views.py
+++ carts/views.py
@@ -48,6 +48,8 @@ def cart(request, quantity=0, cart_items=None,total=0):
         for cart_item in cart_items:
             total +=(cart_item.product.price*cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = tax + total
     except ObjectDoesNotExist :
         pass
 
@@ -57,5 +59,7 @@ def cart(request, quantity=0, cart_items=None,total=0):
         'cart_items':cart_items,
         'total':total,
         'quantity':quantity,
+        'tax':tax,
+        'grand_total':grand_total,
     }
     return render(request, template_name, context)
