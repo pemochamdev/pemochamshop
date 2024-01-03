b7feef63313f5adce4caca3f4f88e4611d4267de
pemochamdev <pemochamdev@gmail.com>
2024-01-03 23:06:18 +0000

    Cart IncrementDecrementRemove with Variations

--- carts/views.py
+++ carts/views.py
@@ -184,24 +184,31 @@ def add_cart(request, product_id):
 
 
 
-def remove_cart(request,product_id):
+def remove_cart(request,product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = get_object_or_404(CartItem, product=product, cart=cart)
-    if cart_item.quantity >1:
+    try:
+        cart_item = get_object_or_404(CartItem, product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity >1:
 
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+       pass
     return redirect('cart')
 
 
-def remove_cart_item(request,product_id):
+def remove_cart_item(request,product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = get_object_or_404(CartItem, product=product, cart=cart)
-    cart_item.delete()
+    try:
+       
+        cart_item = get_object_or_404(CartItem, product=product, cart=cart, id=cart_item_id)
+        cart_item.delete()
+    except:
+       pass
     return redirect('cart')
 
 
